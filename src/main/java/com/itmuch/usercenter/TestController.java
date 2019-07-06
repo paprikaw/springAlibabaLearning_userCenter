15470b493082478f3cd9a50bf1f7957008df79d6
周立 <eacdy0000@126.com>
2019-07-06 21:41:13 +0800

    07-10 多参数请求构造

--- src/main/java/com/itmuch/usercenter/TestController.java
+++ src/main/java/com/itmuch/usercenter/TestController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.Date;
 
 @RestController
@@ -18,8 +17,9 @@ public class TestController {
      * // 面试题
      */
     private final UserMapper userMapper;
+
     @GetMapping("/test")
-    public User testInsert(){
+    public User testInsert() {
         User user = new User();
         user.setAvatarUrl("xxx");
         user.setBonus(100);
@@ -27,6 +27,12 @@ public class TestController {
         user.setUpdateTime(new Date());
         this.userMapper.insertSelective(user);
 
+        return user;
+    }
+
+    // q?id=1&wxId=aaa&...
+    @GetMapping("/q")
+    public User query(User user) {
         return user;
     }
 }
