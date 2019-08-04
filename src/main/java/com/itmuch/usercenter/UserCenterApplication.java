9735d3c95ed1b95ab478051bf86d0d83888ab9ad
周立 <eacdy0000@126.com>
2019-08-04 14:36:32 +0800

    09-16 Spring Cloud Stream-编写消费者

--- src/main/java/com/itmuch/usercenter/UserCenterApplication.java
+++ src/main/java/com/itmuch/usercenter/UserCenterApplication.java
@@ -3,12 +3,15 @@ package com.itmuch.usercenter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 import tk.mybatis.spring.annotation.MapperScan;
 
 // 扫描mybatis哪些包里面的接口
 @MapperScan("com.itmuch")
 @SpringBootApplication
 //@EnableDiscoveryClient
+@EnableBinding(Sink.class)
 public class UserCenterApplication {
 
     public static void main(String[] args) {
